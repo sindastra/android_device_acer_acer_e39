31b4acc89d1db754cec940f373d34ddcbd24ef81
LineageOS <build@debian>
2018-02-09 14:16:48 +0100

    mkvendor.sh: Initial commit of acer_e39

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_acer_e39.mk
---
+++ device_acer_e39.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/acer/acer_e39/acer_e39-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/acer/acer_e39/overlay
+
+LOCAL_PATH := device/acer/acer_e39
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_acer_e39
+PRODUCT_DEVICE := acer_e39
